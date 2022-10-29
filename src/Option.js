abae5f41e41b033e1954029fba75584932ad8b40
Ally Rilling <ally.rilling@gmail.com>
2022-10-28 20:13:28 -0500

    host on firebase

--- src/Option.js
+++
@@ -1,8 +0,0 @@
-export default class Option {
-	constructor(flavor, position, strike, optionPrice) {
-		this.flavor = flavor;
-		this.position = position;
-		this.strike = strike;
-		this.optionPrice = optionPrice;
-	}
-}
