f0ddd9edf6e3f059e1f168a7505449457d25c095
Ally Rilling <ally.rilling@gmail.com>
2022-12-05 15:11:36 -0600

    Add div yield and drift term to BS model

--- src/components/BlackScholes.js
+++ src/components/BlackScholes.js
@@ -7,18 +7,29 @@ export default function BlackScholes() {
 	const [S, setS] = useState(100);
 	const [K, setK] = useState(110);
 	const [sigma, setSigma] = useState(20);
-	const [T, setT] = useState(3 / 12);
+	const [T, setT] = useState(1);
 	const [R, setR] = useState(2);
-	const [callPrice, setCallPrice] = useState(3.921568627450981);
-	const [putPrice, setPutPrice] = useState(11.764705882352937);
+	const [divYield, setDivYield] = useState(0);
 
-	let d1 = 0;
-	let d2 = 0;
-	let Nd1 = 0;
-	let Nd2 = 0;
-	let negNd1 = 0;
-	let negNd2 = 0;
-	let pvK = 0;
+	const [d1, setd1] = useState(0);
+	const [d2, setd2] = useState(0);
+	const [Nd1, setNd1] = useState(0);
+	const [Nd2, setNd2] = useState(0);
+	const [negNd1, setNegNd1] = useState(0);
+	const [negNd2, setNegNd2] = useState(0);
+	const [pvK, setpvK] = useState(0);
+	const [driftTerm, setDriftTerm] = useState(0);
+	const [callPrice, setCallPrice] = useState(0);
+	const [putPrice, setPutPrice] = useState(0);
+
+	const timeUnits = {
+		years: 'Years',
+		months: 'Months -> Years',
+		weeks: 'Weeks -> Years',
+		days365: 'Days (365 days / year) -> Years',
+		days252: 'Days (252 days / year) -> Years',
+	};
+	const [timeUnit, setTimeUnit] = useState(timeUnits.years);
 
 	function NORMDIST(x, mean, sd, cumulative) {
 		return cumulative ? jStat.normal.cdf(x, mean, sd) : jStat.normal.pdf(x, mean, sd);
@@ -27,16 +38,55 @@ export default function BlackScholes() {
 	function doCalcs() {
 		let r = R / 100;
 		let sig = sigma / 100;
-		d1 = (1 / (sig * Math.sqrt(T))) * (Math.log(S / K) + (r + 0.5 * sig ** 2) * T);
-		d2 = d1 - sig * Math.sqrt(T);
-		pvK = K * Math.E ** (-r * T);
-		Nd1 = NORMDIST(d1, 0, 1, true);
-		Nd2 = NORMDIST(d2, 0, 1, true);
-		negNd1 = 1 - Nd1;
-		negNd2 = 1 - Nd2;
+		let dy = divYield / 100;
+		let d1 = (1 / (sig * Math.sqrt(T))) * (Math.log(S / K) + (r - dy + 0.5 * sig ** 2) * T);
+		let d2 = d1 - sig * Math.sqrt(T);
+		let pvK = K * Math.E ** (-r * T);
+		let Nd1 = NORMDIST(d1, 0, 1, true);
+		let Nd2 = NORMDIST(d2, 0, 1, true);
+		let negNd1 = 1 - Nd1;
+		let negNd2 = 1 - Nd2;
 
-		setCallPrice(S * Nd1 - pvK * Nd2);
-		setPutPrice(pvK * negNd2 - S * negNd1);
+		setDriftTerm((r - dy + 0.5 * sig ** 2) * T); // ! idk what drift term should really be, is T included or no?
+
+		setd1(d1);
+		setd2(d2);
+		setNd1(Nd1);
+		setNd2(Nd2);
+		setpvK(pvK);
+		setNegNd1(negNd1);
+		setNegNd2(negNd2);
+
+		let c = S * Math.E ** (-dy * T) * Nd1 - pvK * Nd2;
+		let p = pvK * negNd2 - S * Math.E ** (-dy * T) * negNd1;
+
+		setCallPrice(c);
+		setPutPrice(p);
+	}
+
+	function handleTimeUnitChange(e) {
+		let denominator = 0;
+		switch (e) {
+			case timeUnits.years:
+				denominator = 1;
+				break;
+			case timeUnits.months:
+				denominator = 12;
+				break;
+			case timeUnits.weeks:
+				denominator = 52;
+				break;
+			case timeUnits.days365:
+				denominator = 365;
+				break;
+			case timeUnits.days252:
+				denominator = 252;
+				break;
+			default:
+				denominator = -1;
+		}
+		setT(parseFloat(T) / denominator);
+		setTimeUnit(timeUnits.years);
 	}
 
 	return (
@@ -58,18 +108,6 @@ export default function BlackScholes() {
 					</InputGroup>
 				</Col>
 				<Col>
-					<Form.Label>Strike Price</Form.Label>
-					<InputGroup>
-						<InputGroup.Text>$</InputGroup.Text>
-						<Form.Control
-							value={K}
-							onChange={(event) => {
-								setK(event.target.value);
-							}}
-						/>
-					</InputGroup>
-				</Col>
-				<Col>
 					<Form.Label>Sigma / Volitility</Form.Label>
 					<InputGroup>
 						<Form.Control
@@ -83,6 +121,34 @@ export default function BlackScholes() {
 					</InputGroup>
 				</Col>
 				<Col>
+					<Form.Label>Dividend Yield (Continuously-Compounded)</Form.Label>
+					<InputGroup>
+						<Form.Control
+							value={divYield}
+							onChange={(event) => {
+								let newVal = event.target.value;
+								setDivYield(newVal);
+							}}
+						/>
+						<InputGroup.Text>%</InputGroup.Text>
+					</InputGroup>
+				</Col>
+			</Row>
+			<p></p>
+			<Row>
+				<Col>
+					<Form.Label>Strike Price</Form.Label>
+					<InputGroup>
+						<InputGroup.Text>$</InputGroup.Text>
+						<Form.Control
+							value={K}
+							onChange={(event) => {
+								setK(event.target.value);
+							}}
+						/>
+					</InputGroup>
+				</Col>
+				<Col>
 					<Form.Label>Time</Form.Label>
 					<InputGroup>
 						<Form.Control
@@ -92,7 +158,13 @@ export default function BlackScholes() {
 								setT(newVal);
 							}}
 						/>
-						<InputGroup.Text>years</InputGroup.Text>
+						<Form.Select style={{ backgroundColor: '#eaecef' }} value={timeUnit} onChange={(event) => handleTimeUnitChange(event.target.value)}>
+							<option>{timeUnits.years}</option>
+							<option>{timeUnits.months}</option>
+							<option>{timeUnits.weeks}</option>
+							<option>{timeUnits.days365}</option>
+							<option>{timeUnits.days252}</option>
+						</Form.Select>
 					</InputGroup>
 				</Col>
 				<Col>
@@ -124,6 +196,63 @@ export default function BlackScholes() {
 			</Row>
 			<p></p>
 			<h2>Outputs</h2>
+			<Row>
+				<Col>
+					<Form.Label>d1</Form.Label>
+					<InputGroup>
+						<Form.Control disabled value={d1} />
+					</InputGroup>
+				</Col>
+				<Col>
+					<Form.Label>d2</Form.Label>
+					<InputGroup>
+						<Form.Control disabled value={d2} />
+					</InputGroup>
+				</Col>
+			</Row>
+			<p></p>
+			<Row>
+				<Col>
+					<Form.Label>N(d1)</Form.Label>
+					<InputGroup>
+						<Form.Control disabled value={Nd1} />
+					</InputGroup>
+				</Col>
+				<Col>
+					<Form.Label>N(d2)</Form.Label>
+					<InputGroup>
+						<Form.Control disabled value={Nd2} />
+					</InputGroup>
+				</Col>
+				<Col>
+					<Form.Label>N(-d1)</Form.Label>
+					<InputGroup>
+						<Form.Control disabled value={negNd1} />
+					</InputGroup>
+				</Col>
+				<Col>
+					<Form.Label>N(-d2)</Form.Label>
+					<InputGroup>
+						<Form.Control disabled value={negNd2} />
+					</InputGroup>
+				</Col>
+			</Row>
+			<p></p>
+			<Row>
+				<Col>
+					<Form.Label>PV(K)</Form.Label>
+					<InputGroup>
+						<Form.Control disabled value={pvK} />
+					</InputGroup>
+				</Col>
+				<Col>
+					<Form.Label>Drift Term</Form.Label>
+					<InputGroup>
+						<Form.Control disabled value={driftTerm} />
+					</InputGroup>
+				</Col>
+			</Row>
+			<p></p>
 			<Row>
 				<Col>
 					<Form.Label>Call Price</Form.Label>
